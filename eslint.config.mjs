2aec3c693dcc9c1500d7a502404f723ced677497
TedYin <jjboy830227@gmail.com>
2025-02-25 01:17:00 +0800

    init config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,31 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    // 設定針對所有 js、jsx、ts、tsx 檔案
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    // 載入 prettier 插件
+    plugins: {
+      prettier: require('eslint-plugin-prettier'),
+    },
+    rules: {
+      // 當程式碼與 Prettier 格式不符時，報錯
+      'prettier/prettier': 'error',
+    },
+    // 忽略 node_modules 與 .next 資料夾 以及非 js、jsx、ts、tsx 檔案
+    ignores: ['!**/*.{js,jsx,ts,tsx}', 'node_modules', '.next'],
+    settings: {
+      // Support absolute imports
+      // https://www.npmjs.com/package/eslint-import-resolver-alias
+      'import/resolver': {
+        alias: {
+          map: [['@', './src']],
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+      },
+    },
+  },
 ];
 
 export default eslintConfig;
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    reactCompiler: true, // Enable the new React compiler
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
