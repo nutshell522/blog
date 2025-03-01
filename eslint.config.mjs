275683317c521ed1cd6f49c862acdea0b28a5f5d
TedYin <jjboy830227@gmail.com>
2025-03-02 03:13:47 +0800

    reset eslint and prttier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,43 +1,48 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
-import prettier from 'eslint-plugin-prettier';
-import tailwindcss from 'eslint-config-tailwindcss';
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import tailwind from 'eslint-plugin-tailwindcss'
+import typescriptEslint from '@typescript-eslint/eslint-plugin'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+    baseDirectory: __dirname,
+})
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'plugin:@typescript-eslint/recommended'),
-
-  {
-    files: ['**/*.{js,jsx,ts,tsx}'],
-    plugins: {
-      prettier,
-      tailwindcss,
-      '@typescript-eslint': typescriptEslint,
-    },
-    rules: {
-      'prettier/prettier': 'error', 
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }], 
-      'tailwindcss/classnames-order': 'off', 
-    },
-    settings: {
-      'import/resolver': {
-        typescript: {},
-        alias: {
-          map: [['@', './src']],
-          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    ...compat.extends(
+        'next/core-web-vitals',
+        'plugin:@typescript-eslint/recommended'
+    ),
+    ...tailwind.configs['flat/recommended'],
+    {
+        files: ['**/*.{js,jsx,ts,tsx}'],
+        plugins: {
+            '@typescript-eslint': typescriptEslint,
         },
-      },
+        rules: {
+            'tailwindcss/classnames-order': 'off',
+        },
+        settings: {
+            'import/resolver': {
+                typescript: {},
+                alias: {
+                    map: [['@', './src']],
+                    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+                },
+            },
+        },
+        ignores: [
+            '!**/*.{js,jsx,ts,tsx}',
+            'node_modules',
+            '.next',
+            'contentLayerAdapter.ts',
+        ],
     },
-    ignores: ['!**/*.{js,jsx,ts,tsx}', 'node_modules', '.next', 'contentLayerAdapter.ts'],
-  },
-];
+    eslintConfigPrettier,
+]
 
-export default eslintConfig;
+export default eslintConfig
---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+// import * as tailwindcss from 'prettier-plugin-tailwindcss';
+
+const config = {
+  trailingComma: 'es5',
+  tabWidth: 4,
+  semi: false,
+  singleQuote: true,
+  // plugins: [tailwindcss],
+  // tailwindFunctions: ['clsx']
+};
+
+export default config;
