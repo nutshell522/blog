743819e89688993d424bd54bb25a658e52d3f9b5
TedYin <jjboy830227@gmail.com>
2025-03-02 12:55:45 +0800

    加入程式碼high light

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,36 +1,38 @@
-import { defineDocumentType, makeSource } from 'contentlayer2/source-files';
+import { defineDocumentType, makeSource } from 'contentlayer2/source-files'
+import rehypePrism from 'rehype-prism-plus'
 
 export const Post = defineDocumentType(() => ({
-  name: 'Post',
-  filePathPattern: `content/posts/**/*.mdx`,
-  contentType: 'mdx',
-  fields: {
-    title: {
-      type: 'string',
-      required: true,
+    name: 'Post',
+    filePathPattern: `content/posts/**/*.mdx`,
+    contentType: 'mdx',
+    fields: {
+        title: {
+            type: 'string',
+            required: true,
+        },
+        description: {
+            type: 'string',
+            required: true,
+        },
+        slug: {
+            type: 'string',
+            required: true,
+        },
+        date: {
+            type: 'date',
+            required: true,
+        },
     },
-    description: {
-      type: 'string',
-      required: true,
+    computedFields: {
+        path: {
+            type: 'string',
+            resolve: (post) => `/posts/${post.slug}`,
+        },
     },
-    slug: {
-      type: 'string',
-      required: true,
-    },
-    date: {
-      type: 'date',
-      required: true,
-    },
-  },
-  computedFields: {
-    path: {
-      type: 'string',
-      resolve: (post) => `/posts/${post.slug}`,
-    },
-  },
-}));
+}))
 
 export default makeSource({
-  contentDirPath: 'content',
-  documentTypes: [Post],
-});
+    contentDirPath: 'content',
+    documentTypes: [Post],
+    mdx: { rehypePlugins: [[rehypePrism, { ignoreMissing: true }]] },
+})
